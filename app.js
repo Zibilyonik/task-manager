8d5dcdf9ab93dca5a41f7d4ecc939f94233f1c01
Onur <onatsurmeli@gmail.com>
2023-12-09 00:56:10 +0100

    add express to project, add basic listener and port

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('Task Manager App');
+import express from 'express';
+import tasks from './routes/tasks.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const app = express();
+global.__dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.json());
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World! You\'re on ' + fileURLToPath(import.meta.url) + ' and dirname is ' + __dirname);
+});
+
+app.use('/api/v1/tasks', tasks);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+    console.log(`http://localhost:${port}`);
+});
